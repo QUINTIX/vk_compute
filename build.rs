30a9062c8f087a4917a59b59ee6b9842896e78f7
Patrick Quin <PatrickQuin@yahoo.com>
2022-07-03 21:24:41 -0400

    use glslangvalidator instead of glslc
    glslc seems unavailable from rasbpian apt
    also add build targets

--- build.rs
+++ build.rs
@@ -3,8 +3,8 @@ use std::process::Command;
 const SHADER_LOCATION : &str =  "src/shader.comp";
 
 fn main(){
-	Command::new("glslc").args(&[
-		"-c", SHADER_LOCATION, "-o", "./compute.spv"
+	Command::new("glslangValidator").args(&[
+		"-o", "./compute.spv", "-V100", SHADER_LOCATION
 	]).status().unwrap();
 
 	println!("cargo:rerun-if-changed={}", SHADER_LOCATION);
